63c320e6b63b56d80c020560f2d345c5244ae4e4
Fang Wang <fwangboulder@gmail.com>
2017-02-09 09:51:14 -0800

    add: one pass algorithm

--- #75SortColors.py
+++ #75SortColors.py
@@ -52,4 +52,29 @@ class Solution(object):
         if count_2:
             for i in range(count_0+count_1, count_2+count_1+count_0):
                 nums[i]=2
-            
+
+#method 2: one pass
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n0,n1,n2=-1,-1,-1
+        for i in range(len(nums)):
+            if nums[i]==0:
+                n2+=1
+                nums[n2]=2
+                n1+=1
+                nums[n1]=1
+                n0+=1
+                nums[n0]=0
+            elif nums[i]==1:
+                n2+=1
+                nums[n2]=2
+                n1+=1
+                nums[n1]=1
+            elif nums[i]==2:
+                n2+=1
+                nums[n2]=2
+                
