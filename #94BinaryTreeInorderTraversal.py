85aa4ad487b555efe2bc03ab604333f65852e6df
Fang Wang <fwangboulder@gmail.com>
2017-02-20 22:43:57 -0800

    stack and recursion

---
+++ #94BinaryTreeInorderTraversal.py
@@ -0,0 +1,76 @@
+"""
+94. Binary Tree Inorder Traversal Add to List
+Description  Submission  Solutions
+Total Accepted: 178965
+Total Submissions: 403189
+Difficulty: Medium
+Contributors: Admin
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+Hide Company Tags Microsoft
+Hide Tags Tree Hash Table Stack
+Hide Similar Problems (M) Validate Binary Search Tree (M) Binary Tree Preorder Traversal (H) Binary Tree Postorder Traversal (M) Binary Search Tree Iterator (M) Kth Smallest Element in a BST (H) Closest Binary Search Tree Value II (M) Inorder Successor in BST
+
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            res.append(root.val)
+            self.helper(root.right, res)
+
+#Method 2
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        if root == None: return []
+        r = []; st = []; node = root
+        st.append(root)
+        while st:
+            while node and node.left:
+                st.append(node.left)
+                node = node.left
+            node = st.pop()
+            r.append(node.val)
+            node = node.right
+            if node: st.append(node)
+        return r
