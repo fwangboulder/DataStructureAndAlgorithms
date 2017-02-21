f96342143efbc2bf13ee3413d1739b3d762f7019
Fang Wang <fwangboulder@gmail.com>
2017-02-20 22:35:16 -0800

    dfs

---
+++ #90SubsetsII.py
@@ -0,0 +1,46 @@
+"""
+90. Subsets II Add to List
+Description  Submission  Solutions
+Total Accepted: 97773
+Total Submissions: 284351
+Difficulty: Medium
+Contributors: Admin
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+Hide Company Tags Facebook
+Hide Tags Array Backtracking
+
+#dfs
+
+"""
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        self.dfs(nums, 0, [], res)
+        return res
+
+    def dfs(self, nums, index, path, res):
+        res.append(path)
+        for i in xrange(index, len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums, i+1, path+[nums[i]], res)
