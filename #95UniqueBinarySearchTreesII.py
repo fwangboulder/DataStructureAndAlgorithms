3e04f566cb5ded4d641195dafd55e995461a31f3
Fang Wang <fwangboulder@gmail.com>
2017-02-20 22:46:19 -0800

    recursion

---
+++ #95UniqueBinarySearchTreesII.py
@@ -0,0 +1,50 @@
+"""
+95. Unique Binary Search Trees II Add to List
+Description  Submission  Solutions
+Total Accepted: 75052
+Total Submissions: 244165
+Difficulty: Medium
+Contributors: Admin
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+Hide Tags Tree Dynamic Programming
+Hide Similar Problems (M) Unique Binary Search Trees (M) Different Ways to Add Parentheses
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n == 0:
+            return []
+        return self.dfs(1, n+1)
+
+    def dfs(self, start, end):
+        if start == end:
+            return None
+        result = []
+        for i in xrange(start, end):
+            for l in self.dfs(start, i) or [None]:
+                for r in self.dfs(i+1, end) or [None]:
+                    node = TreeNode(i)
+                    node.left, node.right  = l, r
+                    result.append(node)
+        return result
