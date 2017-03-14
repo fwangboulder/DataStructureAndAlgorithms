b6c9c7b2fc2b39dbcbe1edd6ef84519b91796c04
Fang Wang <fwangboulder@gmail.com>
2017-03-14 07:42:39 -0700

    add: stack

--- #78Subsets.py
+++ #78Subsets.py
@@ -32,12 +32,43 @@ import unittest
 
 class Solution(object):
 
+    def subsets1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        This function produces all possible subsets of nums. Use the iterative method.
+        First, store the index and value in a stack one by one and append it to res array.
+        Example: nums=[1,2,3]
+        First start from 0,
+        stack=[(0,1),(1,2),(2,3)]
+        res=[[1],[1,2],[1,2,3]]
+        Second start from 3: stack=[(0,1),(1,2)]
+        Third start from 2:stack=[(0,1),(2,3)] res+[[1,3]]
+        Fourth start from 1:stack=[(1,2)] res+[[2]]+[[2,3]] +[[3]]+[[]]
+        """
+
+        nums.sort()
+        res, stack, x, n = [[]], [], 0, len(nums)
+        while True:
+            if x < n:
+                stack += [(x, nums[x])]
+                res += [list(zip(*stack)[1])]
+                x += 1
+            elif stack:
+                x = stack.pop()[0] + 1
+            else:
+                return ans
+
+
     def subsets2(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
 
-        this function produces all possible subsets of nums.
+        this function produces all possible subsets of nums. Use depth first search
+        method. Assume nums=[1,2,3] In depth 0, result array append [], which makes [[]]; In depth 1,
+        res=[[],[1],[2],[3]]; In depth 2, res=[[],[1],[2],[3],[1,2],[1,3],[2,3]];
+        In depth 3, res=[[],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
         """
         res = []
         nums.sort()
@@ -56,6 +87,20 @@ test = Solution()
 
 
 class Testsubsets(unittest.TestCase):
+
+    def test_subsets1(self):
+        self.assertEqual(sorted(test.subsets1([1,2,3])), sorted([
+              [3],
+              [1],
+              [2],
+              [1,2,3],
+              [1,3],
+              [2,3],
+              [1,2],
+              []
+            ]))
+        self.assertEqual(sorted(test.subsets1([])), sorted([[]]))
+        self.assertEqual(sorted(test.subsets1([1])), sorted([[1],[]]))
 
     def test_subsets2(self):
         self.assertEqual(sorted(test.subsets2([1, 2, 3])), sorted([
