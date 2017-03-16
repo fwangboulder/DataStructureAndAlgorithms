4f2f2a75d597557a27a1b50c3e3c1b6da5d797b2
Fang Wang <fwangboulder@gmail.com>
2017-03-15 17:16:31 -0700

    add: search insert position, Binary Search, unit test

--- BinarySearch.py
+++ BinarySearch.py
@@ -7,6 +7,13 @@ Given a sorted array, find the start and end position of a given target value.
 
 example: [5,7,7,8,8,10] return [3,4]
 if not found, return [-1,-1]
+
+
+Search insert position(#35)
+Given a sorted array and a target value, return the index if the target is found.
+if not, return the index where it would be it were inserted in order.
+example:
+[1,3,5,6], 5 -> 2
 """
 
 
@@ -32,8 +39,8 @@ class Solution(object):
         index1 = -1
         index2 = -1
         # find the left bound of target
-        while start + 1 < end:  #avoid dead loop
-            mid = start + (end - start) / 2  #avoid exceed value limit
+        while start + 1 < end:  # avoid dead loop
+            mid = start + (end - start) / 2  # avoid exceed value limit
             if nums[mid] > target:
                 end = mid
             elif nums[mid] < target:
@@ -64,6 +71,40 @@ class Solution(object):
 
         return [index1, index2]
 
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        this function produces the position of the target, if the target not found, return the
+        insert position which keep the nums in order.
+        In another word, this is a problem to find the first value equal or bigger than the target valueand
+        then return the index. I can use the two pointers and get rid of half part of elements by comparision of values for
+        target and mid=start+(end-start)/2. The time complexity is O(logn) and the space complexity is O(1).
+        """
+        if not nums:
+            return 0
+        # check the case that target is the smallest or largest value
+        if target < nums[0]:
+            return 0
+        if target > nums[-1]:
+            return len(nums)
+        start = 0
+        end = len(nums) - 1
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            if nums[mid] > target:
+                end = mid
+            elif nums[mid] < target:
+                start = mid
+            else:
+                end = mid  # the first value equal or larger than target
+        if nums[start] >= target:
+            return start
+        if nums[end] >= target:
+            return end
+
+
 # test
 case = Solution()
 
@@ -73,6 +114,12 @@ class test(unittest.TestCase):
     def test_searchRange(self):
         self.assertEqual(case.searchRange([5, 7, 7, 8, 8, 10], 8), [3, 4])
         self.assertEqual(case.searchRange([5, 7, 7, 8, 8, 10], 0), [-1, -1])
+
+    def test_searchInsert(self):
+        self.assertEqual(case.searchInsert([1], 2), 1)
+        self.assertEqual(case.searchInsert([2], 2), 0)
+        self.assertEqual(case.searchInsert([1, 2, 4], 3), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
