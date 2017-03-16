f806cea7afd841e0cdb1caf1e02dee7ab6ea6b6e
Fang Wang <fwangboulder@gmail.com>
2017-03-15 18:32:58 -0700

    add:search 2D matrix, binary search, unit test, step-wise search

--- BinarySearch.py
+++ BinarySearch.py
@@ -14,6 +14,15 @@ Given a sorted array and a target value, return the index if the target is found
 if not, return the index where it would be it were inserted in order.
 example:
 [1,3,5,6], 5 -> 2
+
+Search a 2D Matrix (#74) for a target value
+Given a 2D matrix: integers in each row are sorted from left to right; the first integer is always
+larger than the last integer in previous row.
+
+Search a 2D Matrix II (#240)
+Given a 2D matrix: integers in each row are sorted for each row and each column, which means the
+first integer might be smaller than the last integer in previous row.
+
 """
 
 
@@ -104,6 +113,76 @@ class Solution(object):
         if nums[end] >= target:
             return end
 
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        This function produces whether the matrix contains the target. Notice the matrix
+        is sorted.
+        I can treat the matrix as a one dimension sorted array basing on the property of
+        the matrix. Then I want to use two pointers to search for the target.
+        Binary search method will give me a time complexity of O(log(mn)). The space complexity
+        is O(1).
+        """
+        if not matrix or not matrix[0]:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        end = m * n - 1
+        start = 0
+        # check if target is smaller than the smallest value or larger than the
+        # largest value
+        if matrix[0][0] > target:
+            return False
+        if matrix[m - 1][n - 1] < target:
+            return False
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            row = mid / n
+            col = mid % n
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                end = mid
+            else:
+                start = mid
+        if matrix[start /
+                  n][start %
+                     n] == target or matrix[end /
+                                            n][end %
+                                               n] == target:
+            return True
+        else:
+            return False
+
+    def searchMatrix2(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+
+        This function produces whether the matrix contains target. Basing on the property of
+        of the matrix. I will start from the left down corner and do a step-wise search for
+        target. The time complexity is O(n) and the space complexity is O(1).
+        """
+        if not matrix or not matrix[0]:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        # start from left down side
+        row = m - 1
+        col = 0
+        while row >= 0 and col < n:
+            print matrix[row][col]
+            # check to step right or up
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                row -= 1
+            else:
+                col += 1
+        return False
 
 # test
 case = Solution()
@@ -121,7 +200,20 @@ class test(unittest.TestCase):
         self.assertEqual(case.searchInsert([1, 2, 4], 3), 2)
         self.assertEqual(case.searchInsert([1, 2, 2, 4], 2), 1)
 
+    def test_searchMatrix(self):
+        self.assertEqual(case.searchMatrix(
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 8), True)
+        self.assertEqual(case.searchMatrix(
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 10), False)
+        self.assertEqual(case.searchMatrix(
+            [[1, 2, 6], [7, 9, 11], [13, 15, 19]], 8), False)
 
+    def test_searchMatrix2(self):
+        self.assertEqual(case.searchMatrix(
+            [[1, 2, 3], [2, 3, 4], [3, 4, 5]], 1), True)
+        self.assertEqual(case.searchMatrix(
+            [[1, 2, 3], [2, 3, 4], [3, 4, 5]], 6), False)
+        self.assertEqual(case.searchMatrix([[]], 6), False)
 
 if __name__ == "__main__":
     unittest.main()
