2b047666e4ae97dc3bffce659cff631511b71fdd
Fang Wang <fwangboulder@gmail.com>
2017-03-19 15:35:26 -0700

    add: comments

--- #270ClosestBinarySearchTreeValue.py
+++ #270ClosestBinarySearchTreeValue.py
@@ -1,3 +1,10 @@
+#! /usr/bin/env python
+#############################
+###by Fang Wang #############
+###March 19 2017########
+###########################
+
+import unittest
 """
 270. Closest Binary Search Tree Value   Add to List QuestionEditorial Solution  My Submissions
 Total Accepted: 27396
@@ -23,26 +30,31 @@ class Solution(object):
         :type root: TreeNode
         :type target: float
         :rtype: int
+        This function returns the value in the binary search tree that is closest to
+        the target. Use the recursion method. The time complexity is O(logn) and
+        the space compexity is O(1).
         """
         if not root:
             return None
-
-        result, diff = self.helper(root, target)
+        #use a helper function to return the res and diff between target and res
+        result, diff = self.ClosestValueHelper(root, target)
         return result
 
-    def helper(self, root, target):
+    def ClosestValueHelper(self, root, target):
+
         result = root.val
         diff = abs(root.val - target)
-
+        #target is larger than root.val, go right
         if target >= root.val:
             if root.right:
-                rightResult, rightDiff = self.helper(root.right, target)
+                rightResult, rightDiff = self.ClosestValueHelper(root.right, target)
                 if diff > rightDiff:
                     result = rightResult
                     diff = rightDiff
+        #if target is smaller than root.val, go left
         else:
             if root.left:
-                leftResult, leftDiff = self.helper(root.left, target)
+                leftResult, leftDiff = self.ClosestValueHelper(root.left, target)
                 if diff > leftDiff:
                     result = leftResult
                     diff = leftDiff
